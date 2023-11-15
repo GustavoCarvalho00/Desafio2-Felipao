d100bcf56da73f9766f960e792f7ee5869fd249a
João Gustavo <150349750+GustavoCarvalho00@users.noreply.github.com>
2023-11-15 17:41:52 -0300

    Update calculandoVitorias.js

--- calculandoVitorias.js
+++ calculandoVitorias.js
@@ -1,44 +1,43 @@
 const prompt = require('prompt-sync')();
 
-let vitorias = prompt("Digite seu numero de vitorias ")
-let derrotas = prompt("Digite seu numero de derrotas ")
-let rank = calculandoRank(vitorias,derrotas)
+let vitorias = prompt("Digite seu numero de vitorias ");
+let derrotas = prompt("Digite seu numero de derrotas ");
+let rank = calculandoRank(vitorias, derrotas);
 
-function calculandoRank(vitorias,derrotas){
-    let eloRank = (vitorias - derrotas)
-    return eloRank
-
+function calculandoRank(vitorias, derrotas) {
+    let eloRank = vitorias - derrotas;
+    return eloRank;
 }
 
 switch (true) {
-    case vitorias != 0:
-        console.log("você atualmente é Ferro")
-        break
-    
-    case vitorias > 10 && vitorias <= 20:
-        console.log("você atualmente é Bronze")
-        break
-    
-    case vitorias > 20 && vitorias <= 50:
-        console.log("você atualmente é Prata")
-        break
-    
-    case vitorias > 50 && vitorias <= 80:
-        console.log("você atualmente é Ouro")
-        break
-    
-    case vitorias > 80 && vitorias <= 90:
-        console.log("você atualmente é Diamante")
-        break
-    
-    case vitorias > 91 && vitorias <= 100:
-        console.log("você atualmente eh Lendário")
-        break
-    
-    case vitorias >= 101:
-        console.log("voce atualmente eh Imortal")
-        break
-    
+    case rank > 0 && rank <= 10:
+        console.log("Você atualmente é Ferro e tem " + vitorias + " vitórias");
+        break;
+
+    case rank > 10 && rank <= 20:
+        console.log("Você atualmente é Bronze e tem " + vitorias + " vitórias");
+        break;
+
+    case rank > 20 && rank <= 50:
+        console.log("Você atualmente é Prata e tem " + vitorias + " vitórias");
+        break;
+
+    case rank > 50 && rank <= 80:
+        console.log("Você atualmente é Ouro e tem " + vitorias + " vitórias");
+        break;
+
+    case rank > 80 && rank <= 90:
+        console.log("Você atualmente é Diamante e tem " + vitorias + " vitórias");
+        break;
+
+    case rank > 91 && rank <= 100:
+        console.log("Você atualmente é Lendário e tem " + vitorias + " vitórias");
+        break;
+
+    case rank >= 101:
+        console.log("Você atualmente é Imortal e tem " + vitorias + " vitórias");
+        break;
+
     default:
-        console.log("Parabéns você é muito ruim")
+        console.log("Parabéns, você é muito ruim");
 }
